4c7d7861510249f3d8a47cfb2605489e473eef2c
Petre Leonard Macamete <petreleon2309@yahoo.com>
2022-12-04 13:13:10 +0200

    day 4

---
+++ day04_02.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ifstream file("input.txt");
+    string temp;
+    char lineSymbol, commaSymbol;
+    int counter = 0;
+    while (file >> temp)
+    {
+        stringstream lineStream(temp);
+        int firstStart, firstEnd, secondStart, secondEnd;
+        lineStream >> firstStart >> lineSymbol >> firstEnd >> commaSymbol >> secondStart >> lineSymbol >> secondEnd;
+        if (firstEnd <= secondEnd && firstEnd >= secondStart || firstStart <= secondEnd && firstStart >= secondStart || firstStart >= secondStart && firstEnd <= secondEnd || secondStart >= firstStart && secondEnd <= firstEnd)
+            counter++;
+    }
+    cout << counter << endl;
+    return EXIT_SUCCESS;
+    
+}
