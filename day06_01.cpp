4ae518c46a49d7b467e408e6af6907001a261c82
Petre Leonard Macamete <petreleon2309@yahoo.com>
2022-12-07 16:36:22 +0200

    day 06

---
+++ day06_01.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ifstream file("input.txt");
+    string temp;
+    while (getline(file, temp))
+    {
+        for(int i = 4; i <= temp.size(); i += 1){
+            map<char, bool> symbols;
+            bool isUnique = true;
+            for(int j = i-4; j < i; j += 1){
+                if (symbols[temp[j]]){
+                    isUnique = false;
+                    break;
+                }
+                symbols[temp[j]] = true;
+            }
+            if (isUnique){
+                cout << i << endl;
+                break;
+            }
+        }
+    }
+    
+    return EXIT_SUCCESS;
+}
